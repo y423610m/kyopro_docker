15a938d5adfb34c7ef62a43c47d15ef8adccefda
y423610m <pixy@thinkpad202412.>
2024-12-09 02:10:28 +0900

    fix typo

--- setup.bash
+++ setup.bash
@@ -1,4 +1,4 @@
 MY_KYOPRO_DOCKER_ROOT=$(pwd)
 echo "#--- kyopro docker scripts (${MY_KYOPRO_DOCKER_ROOT}) ---" >> ~/.bashrc
 echo "export MY_KYOPRO_DOCKER_ROOT=${MY_KYOPRO_DOCKER_ROOT}" >> ~/.bashrc 
-echo "source MY_KYOPRO_DOCKER_ROOT/.bashrc" >> ~/.bashrc 
+echo "source ${MY_KYOPRO_DOCKER_ROOT}/.bashrc" >> ~/.bashrc 
